5079ddd33f2e75b8a5c6a07fce0cc5c269631977
AskatDzhambulov <askat.dzhambulov@iaau.edu.kg>
2019-12-20 09:49:07 +0600

    addded btn and lose command

--- game.py
+++ game.py
@@ -15,14 +15,13 @@ def but():
             x = x+33
         y = y+27
 
-but()
-
 faq = '''Let`s play'''
 
 canvas.create_text(310, 240, text=faq, fill="purple", font=("Helvetica", "14"))
 words = ["chazzan", "chazzen", "fuzzing", "buzzing", "frizzly", "muzzily", "fizzily"]
 
 def arr():
+    but()
     word = random.choice(words)
     wo = word[1:-1]
     wor = []
@@ -49,24 +48,71 @@ def arr():
             b2 = list1[ind]
             wor[ind] = '1'
 
+            def kord():
+                if b2 ==1:
+                    x1,y1 = 315, 40
+                if b2 ==2:
+                    x1,y1 = 347, 40
+                if b2 ==3:
+                    x1,y1 = 380, 40
+                if b2 ==4:
+                    x1,y1 = 412, 40
+                if b2 ==5:
+                    x1,y1 = 444, 40
+                if b2 ==6:
+                    x1,y1 = 477, 40
+                return x1, y1
+
+            x1, y1 = kord()
+
+            win.append(v)
+            a2 = canvas.create_text(x1, y1, text=wo[ind], fill="purple", font=("Helvetica", "18"))
+            btn[key] ["bg"] = "green"
             if not v in wor:
-                bnt[key] ["state"] = "disabled"
+                btn[key] ["state"] = "disabled"
             if v in wor:
                 win.append(v)
                 ind2 = wor.index((v))
                 b2 = list1[ind2]
                 canvas.create_text(150, 150, text = "You win!", fill="purple", font=("Helvetica", "18"))
+                for i in  alfabet:
+                    btn[i] ["state"] = "disabled"
+        else:
+            error.append(v)
+            btn[key] ["bg"] = "red"
+            btn[key] ["state"] = "disabled"
 
-    bnt = {}
 
-    def gen(u, x, y, btn=None):
+    btn = {}
+
+    def gen(u, x, y):
         btn[u] = Button(root, text=u, width=3, height=1, command=lambda: a(u))
         btn[u].place(x=str(x), y=str(y))
+    x = 265
+    y = 110
+    for i in alfabet[0:8]:
+        gen(i, x, y)
+        x = x+ 33
+    x = 265
+    y = 137
+    for i in alfabet[8:16]:
+        gen(i, x, y)
+        x = x+ 33
+    x = 265
+    y = 164
+    for i in alfabet[16:24]:
+        gen(i, x, y)
+        x = x+ 33
+    x = 265
+    y = 191
+    for i in alfabet[24:26]:
+        gen(i, x, y)
+        x = x + 33
 
 
 btn01 = Button(root, text="Start!", width=10, height=2, command=lambda: arr( ))
 btn01.place(x=258, y=542)
-btn01["bg"] = "red"
+btn01 ["bg"] = "red"
 
 
 root.mainloop()
